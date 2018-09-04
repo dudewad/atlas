f141c19db38b85faab756d6f07d51ff09d0f32b0
dudewad <me@laytonmiller.com>
2018-09-03 19:06:34 -0700

    Finesse for header. Final tweaks on footer as well.

--- src/app/containers/app-header/app-header.component.ts
+++ src/app/containers/app-header/app-header.component.ts
@@ -1,32 +1,11 @@
-import { BreakpointObserver } from '@angular/cdk/layout';
-import {
-  ChangeDetectionStrategy,
-  ChangeDetectorRef,
-  Component,
-  OnDestroy,
-  OnInit,
-} from '@angular/core';
-import { takeUntil } from 'rxjs/operators';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 import {
   StickyHeaderItem,
   StickyHeaderItemActionType,
 } from 'atlas-lib/sticky-header';
-import { BreakpointConfig, Dictionary } from 'atlas-lib/utils';
 
 import { BpCfg } from 'app/utils';
-import { Subject } from 'rxjs/index';
-
-/**
- * These should be key/value where the value is the name of the svg icon string
- * to be passed to mat-icon (so they must be valid icon names!)
- * @type Dictionary<string>
- */
-const logos: Dictionary<string> = {
-  logoFull: 'logo-full',
-  logoTypographic: 'logo-typographic',
-  logoTypographicSmall: 'logo-typographic-small',
-};
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -34,10 +13,9 @@ const logos: Dictionary<string> = {
   styleUrls: ['./app-header.component.scss'],
   templateUrl: './app-header.component.html',
 })
-export class AppHeaderComponent implements OnDestroy, OnInit {
-  public logoIcon: string = logos.logoFull;
-
-  private destroy$ = new Subject<void>();
+export class AppHeaderComponent {
+  public bpCfg = BpCfg;
+  // private destroy$ = new Subject<void>();
 
   public menuItems: StickyHeaderItem[] = [
     {
@@ -83,38 +61,4 @@ export class AppHeaderComponent implements OnDestroy, OnInit {
       },
     },
   ];
-  public bpCfg: BreakpointConfig = BpCfg;
-
-  public constructor(private breakpointObs: BreakpointObserver,
-                     private cdr: ChangeDetectorRef) {
-  }
-
-  /**
-   * NgOnDestroy Interface Method
-   */
-  public ngOnDestroy() {
-    this.destroy$.next();
-  }
-
-  /**
-   * NgOnInit
-   */
-  public ngOnInit() {
-    this.breakpointObs
-    .observe([BpCfg.lgMin, BpCfg.mdMin])
-    .pipe(takeUntil(this.destroy$))
-    .subscribe(() => this.onBreakpointChange());
-  }
-
-  private onBreakpointChange() {
-    if (this.breakpointObs.isMatched(BpCfg.lgMin)) {
-      this.logoIcon = logos.logoFull;
-    } else if (this.breakpointObs.isMatched(BpCfg.mdMin)) {
-      this.logoIcon = logos.logoTypographic;
-    } else {
-      this.logoIcon = logos.logoTypographicSmall;
-    }
-
-    this.cdr.markForCheck();
-  }
 }
